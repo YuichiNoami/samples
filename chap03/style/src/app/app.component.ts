53d853c787f85d94af9d50d4b0877bc0779a3bc3
Yuichi Noami <ynoami@2xjack.com>
2021-08-09 14:56:56 +0900

    スタイルバインディングを学習

--- chap03/style/src/app/app.component.ts
+++ chap03/style/src/app/app.component.ts
@@ -3,18 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <div [style.background-color]="bcolor">WINGSプロジェクト</div>
-  `
-
-  // 式の値によって、オンオフを切り替えたい場合
-  /*
-  template: `
     <div [style.background-color]="bcolor ? '#0ff' : ''">
       WINGSプロジェクト</div>
   `
-  */
 })
 export class AppComponent {
-   bcolor = '#0ff';
-  // bcolor = true;
+  bcolor = false;
 }
